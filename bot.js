7ff45c910e1538a3e3fe3af35d8dc5e476138236
TheeDeeRob <dannyrobinsonjr87@gmail.com>
2016-04-08 14:16:44 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ function respond() {
       botRegex = /^\/cool guy/;  botRegexDL = /^\/DDL/i;botRegexSalt = /^\/salt/;botRegexRules = /^\/rules/
       botRegexAd=/^\/advance/;botRegexGTA = /^\/gta/; botRegexSC = /^\/SDL/i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botDuck = /^\/duck/;
       botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexSb = /^\/sub/; botRegexSh = /^\/shrug/; botRegexWk = /^\/users/; botRegexCC = /^\/cc/;
-      botRegexSiege = /^\/siege/
+      botRegexSiege = /^\/siege/; botRegexSC = /^\/ST/ibotRegexSC = /^\/SDL/i
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
@@ -151,3 +151,15 @@ function getRandomInt(min, max) {
 
 
 exports.respond = respond;
+
+else if(request.text && botST.test(request.text)) {
+    this.res.writeHead(200);
+    var team= request.txt.substring (4,9)
+    if(team) {
+      if (team == "nycfc") {
+        postMessage("https://www.youtube.com/channel/UCh4--hcRHxKmOn5BHIy3GyA");
+     } else if (team == "nyrb") {
+        postMessage("google.com");
+     }
+    }
+    
