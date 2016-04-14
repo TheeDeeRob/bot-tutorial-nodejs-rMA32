3b5b19d2e61e92148a740e567ebf4d3a1c6730ab
TheeDeeRob <dannyrobinsonjr87@gmail.com>
2016-04-14 11:40:56 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ function respond() {
       botSquad= /^\/sq/i;botRegex = /^\/cool guy/; botRegexDLRecAvg = /^\/rec avg/;  botRegexDL = /^\/DDL/i;botCga = /^\/cga/;botRegexRules = /^\/rules/
       botRegexAd=/^\/advance/;botRegexGTA = /^\/gta/; botRegexSC = /^\/SDL/i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botDuck = /^\/duck/;
       botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexSb = /^\/sub/; botRegexSh = /^\/shrug/; botRegexWk = /^\/users/; botRegexCC = /^\/cc/;
-      botRegexSiege = /^\/siege/; botDoc = /^\/doc/; botSf = /^\/sf/;  botSd = /^\/sd/;  botBaseball = /^\/baseball/; botSteve = /^\/steve/;
+      botRegexSiege = /^\/siege/; botDoc = /^\/doc/; botSf = /^\/sf/;  botSd = /^\/sd/;  botDRAFT = /^\/draft/; botSteve = /^\/steve/;
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
@@ -28,9 +28,9 @@ function respond() {
     }
     this.res.end();
   }
-    else if (request.text && botBaseball.test(request.text)) {
+    else if (request.text && botDRAFT.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://24.media.tumblr.com/tumblr_l1vkvxS0zo1qa9armo1_500.jpg");
+    postMessage("https://docs.google.com/spreadsheets/d/1RLt5e2PMHEOoVij3S4b1Y7CYzqpw6bTsfGdP8-ACQoY/pubhtml");
     this.res.end();
   }
   else if (request.text && botSd.test(request.text)) {
