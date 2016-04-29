97488c282d10a7b68062712725821f3ede54f846
TheeDeeRob <dannyrobinsonjr87@gmail.com>
2016-04-29 11:30:46 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ function respond() {
   var request = JSON.parse(this.req.chunks[0]),botHugs = /^\/hugs/;
       botSquad= /^\/sq/i;botRegex = /^\/cool guy/; botRegexDLRecAvg = /^\/rec avg/;  botRegexDL = /^\/DDL/i;botCga = /^\/cga/;botRegexRules = /^\/rules/
       botRegexApp=/^\/app/;botRegexGTA = /^\/gta/; botRegexSC = /^\/SDL/i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botTable = /^\/table/;
-      botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexSb = /^\/sub/; botRegexSh = /^\/shrug/; botRegexWk = /^\/users/; botRegexCC = /^\/cc/;
+      botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexSb = /^\/usopen/; botRegexSh = /^\/shrug/; botRegexWk = /^\/users/; botRegexCC = /^\/cc/;
       botRegexSiege = /^\/siege/; botGoldenBoot = /^\/goldenboot/; botSf = /^\/sf/;  botSd = /^\/sd/;  botDRAFT = /^\/draft/; botSteve = /^\/steve/;
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
@@ -94,7 +94,7 @@ function respond() {
   } 
   else if(request.text && botRegexSb.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://www.reddit.com/r/maddenall32");
+    postMessage("https://www.enjore.com/en/tournament/45821/fil-usa-open-cup/stream/");
     this.res.end();
   } 
   else if(request.text && botRegexSh.test(request.text)) {
